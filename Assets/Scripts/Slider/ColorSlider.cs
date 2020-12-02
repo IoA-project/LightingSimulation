c896d0c4a119c3454b7d853f865dcb6a63f694e9
chicchi8 <c1050383@planet.kanazawa-it.ac.jp>
2020-12-02 22:24:31 +0900

    ライトのスライダーを追加

---
+++ Assets/Scripts/Slider/ColorSlider.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class ColorSlider : MonoBehaviour
+{
+    [SerializeField] GameObject ChangeLight;
+    public GameObject lightObject;
+    
+    public Slider sliderRed;
+    public Slider sliderGreen;
+    public Slider sliderBlue;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        var children = new List<Transform>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+    }
+    public void ValueChange()
+    {
+        float x = sliderRed.value;
+        float y = sliderGreen.value;
+        float z = sliderBlue.value;
+        foreach (Transform childTransform in ChangeLight.transform)
+        {
+            childTransform.GetComponent<Light>().color = new Color(x, y, z, 1);
+        }
+    }
+}
