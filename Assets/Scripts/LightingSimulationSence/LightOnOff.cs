40209807b613233437818fc3be89dae00a103693
rainierrr <b1916320@planet.kanazawa-it.ac.jp>
2021-01-08 20:09:07 +0900

    微修正

--- Assets/Scripts/LightingSimulationSence/LightOnOff.cs
+++ Assets/Scripts/LightingSimulationSence/LightOnOff.cs
@@ -2,11 +2,9 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class LightOnOff: MonoBehaviour 
+public class LightOnOff: MonoBehaviour
 {
     public GameObject LightGroup1;
-    private bool LightOn;
-
 
     // Start is called before the first frame update
     void Start()
