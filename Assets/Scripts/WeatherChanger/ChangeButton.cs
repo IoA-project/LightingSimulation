7cdf9a2c19a05cbb16bae0107168d98262c4941a
kaito0817 <b1902068@planet.kanazawa-it.ac.jp>
2021-01-15 19:10:32 +0900

    WeatherchangeButtonの追加

---
+++ Assets/Scripts/WeatherChanger/ChangeButton.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeButton : MonoBehaviour
+{
+    public GameObject particle;
+    public int a = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    public void Onclick()
+    {
+        if (a == 0)
+        {
+            Instantiate(particle, transform.position, transform.rotation);
+            a++;
+        }
+        else if(a == 1)
+        {
+            GameObject obj = GameObject.Find("Snow(Clone)");
+            Destroy(obj);
+            a--;
+        }
+    }
+    void Update()
+    {
+        
+    }
+}
