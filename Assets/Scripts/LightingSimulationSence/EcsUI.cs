0b07a9040209983481aeb6f94c598d5e3ee0addb
rainierrr <b1916320@planet.kanazawa-it.ac.jp>
2021-02-12 00:45:12 +0900

    天候変更完了

--- Assets/Scripts/LightingSimulationSence/EcsUI.cs
+++ Assets/Scripts/LightingSimulationSence/EcsUI.cs
@@ -7,12 +7,15 @@ using UnityEngine;
 public class EcsUI : MonoBehaviour
 {
     public GameObject GameManager;
+    public GameObject SummerMusic;
+    public Material WinterSkyBox;
+    public Material SummerSkyBox;
     private GameManager gm;
     // Start is called before the first frame update
     void Start()
     {
-        gm = GameManager.GetComponent<GameManager>();
 
+        gm = GameManager.GetComponent<GameManager>();
     }
 
     // Update is called once per frame
@@ -20,11 +23,15 @@ public class EcsUI : MonoBehaviour
     {
         if (gm.ShowState)
         {
+            SummerMusic.SetActive(true);
+            RenderSettings.skybox = SummerSkyBox;
             GameObject obj = GameObject.Find("Snow(Clone)");
             Destroy(obj);
         }
         else
         {
+            SummerMusic.SetActive(false);
+            RenderSettings.skybox = WinterSkyBox;
             Instantiate(gm.ShowPrefab, gm.ShowPrefab.transform.position, gm.ShowPrefab.transform.rotation);
         }
         gm.ShowState = !gm.ShowState;
